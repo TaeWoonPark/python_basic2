c74fd958f32b5adb8e6e910af4590fee6a68fc79
TaeWoonPark <yasuoa19@gmail.com>
2025-08-28 16:50:36 +0900

    課題B再々々提出

--- b_3.py
+++ b_3.py
@@ -4,7 +4,7 @@ cols = int(input("列数を入力してください: "))
 for j in range(1, rows + 1):
     for i in range(1, cols + 1):
         # 式全体を18文字に固定（100以上にも対応）
-        print(f"{i} x {j} = {i*j:^3}", end="|")
+        print(f" {i} x {j} = {i*j:^3}", end="|")
     print()
 
 
